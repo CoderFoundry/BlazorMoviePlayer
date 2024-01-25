69c62cb2b1d2baa7e9f63303060ed3e20a240710
bdavisjr <bdavis@core-techs.net>
2024-01-25 17:45:06 -0500

    CSS and Styling

--- Components/Pages/MoviePlayer.razor.js
+++ Components/Pages/MoviePlayer.razor.js
@@ -4,8 +4,6 @@
     vplayer.src = `/Videos/${fileName}`;   
     vplayer.load();       
     vplayer.play();
-    
-    
 }
 
 export function loadMovie(fileName) {
@@ -15,17 +13,4 @@ export function loadMovie(fileName) {
 }
 
 
-export function fade(opts) {
-    let timer = void 0;
-    let vplayer = document.getElementById("videoPlayer");
-    if (typeof timer !== 'undefined') clearInterval(timer);
-    var opacity = opts === 'in' ? 0 : 1;
-    opacityTimer = setInterval(function () {
-        if ((opts === 'in' && opacity >= 1) || (opts === 'out' && opacity <= 0)) {
-            clearInterval(timer);
-        }
-        opacity = opts === 'in' ? opacity + (opacity * 0.1 || 0.1) : opacity - (opacity * 0.1 || 0.1);
-        vplayer.style.opacity = opacity;
-        vplayer.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
-    }, 25);
-}
+
