4681d8edefeb92ba7978630fa6f358f78dfd7f94
bdavisjr <bdavis@core-techs.net>
2024-01-26 13:10:05 -0500

    add selected css styling to movie card.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,10 +1,16 @@
-﻿namespace BlazorMoviePlayer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMoviePlayer.Models
 {
     public class Movie
     {
+        [Required]    
         public string? Title { get; set; }
+        [Required]
         public string? Poster { get; set; }
+        [Required]
         public string? Video { get; set; }
+       
         
     }
 }
